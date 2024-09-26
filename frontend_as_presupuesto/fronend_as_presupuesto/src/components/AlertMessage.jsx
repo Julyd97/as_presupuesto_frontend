ba0bd3cce205660610a21da45e58e15b671401e0
julyd97 <julyd97@gmail.com>
2024-09-25 22:21:31 -0500

    Alert message style

--- frontend_as_presupuesto/fronend_as_presupuesto/src/components/AlertMessage.jsx
+++ frontend_as_presupuesto/fronend_as_presupuesto/src/components/AlertMessage.jsx
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 import { Alert, Button } from "react-bootstrap"; 
   
 function AlertMessage({show}) {
+    const mystyle = {  
+        position : "fixed",
+        top : "0",
+        width : "100%",
+        zIndex : "1056",
+    };
     return ( 
         <div> 
             {show && ( 
                 <Alert 
+                    style={mystyle}
                     variant="danger"
                     onClose={() => setShow(false)} 
                     dismissible
                     fade> 
                       
-                    <Alert.Heading style={{}}>Error</Alert.Heading> 
+                    <Alert.Heading>Error</Alert.Heading> 
                     <p>Error message goes here.</p> 
                 </Alert> 
             )} 
